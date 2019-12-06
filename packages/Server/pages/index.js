ec3999d6a95748d4bc24cbb5da6300e018befbe1
oqx <jay.kariesch@gmail.com>
2019-12-06 15:15:15 -0600

    updated files field in package.json for loader. added package to server

--- packages/Server/pages/index.js
+++ packages/Server/pages/index.js
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic'
 import React from 'react'
 
-const Component = dynamic(() => import('../components'), {
+const Component = dynamic(() => import('@oqx/micro-frontend'), {
     ssr: false
 })
 const Page = (props) => {
